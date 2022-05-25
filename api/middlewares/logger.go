070b67d582fa9f1e787f5555958b7747d42c80dd
Hafiz Tsalavin <muhammad.hafiz@intern.bukalapak.com>
2022-05-25 19:47:03 +0700

    alvin : add log still stuck how to log properly

--- api/middlewares/logger.go
+++ api/middlewares/logger.go
@@ -1,27 +1,12 @@
 package middlewares
 
 import (
-	"log"
 	"net/http"
-	"time"
-
-	"simple-jwt-go/api/logger"
 )
 
 func Log(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		lrw := logger.NewLoggingResponseWriter(w)
-
-		next.ServeHTTP(lrw, r)
-
-		statusCode := lrw.StatusCode
-		log.Printf(
-			"%s\t%s\t%d\t%s",
-			r.Method,
-			r.RequestURI,
-			statusCode,
-			time.Since(start),
-		)
+		// log
+		next.ServeHTTP(w, r)
 	})
 }
--- go.mod
+++ go.mod
@@ -30,6 +30,9 @@ require (
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
